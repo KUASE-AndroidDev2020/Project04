c99184edc6fccd9fc506cb1887eacaef6ec284c6
田中大誓 <2020m062@kuas.ac.jp>
2020-06-24 22:43:56 +0900

    added home buttons

--- app/src/main/java/com/example/sportstimer/IntervalActivity.java
+++ app/src/main/java/com/example/sportstimer/IntervalActivity.java
@@ -2,7 +2,9 @@ package com.example.sportstimer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class IntervalActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class IntervalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interval);
+    }
+
+    public void IntervalBackHome(View view){
+        Intent toBackHome = new Intent(this,MainActivity.class);
+        startActivity(toBackHome);
     }
 }
--- app/src/main/java/com/example/sportstimer/TimerActivity.java
+++ app/src/main/java/com/example/sportstimer/TimerActivity.java
@@ -3,7 +3,9 @@ package com.example.sportstimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class TimerActivity extends AppCompatActivity {
 
@@ -11,5 +13,10 @@ public class TimerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
+    }
+    public void TimerBackHome(View view){
+        Intent toBackHome = new Intent(this,MainActivity.class);
+        startActivity(toBackHome);
+
     }
 }
