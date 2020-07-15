b72ce1c08d84e2e746cd407a67020b6c772c763d
morotamorota <2020m086@kuas.ac.jp>
2020-07-15 20:13:30 +0900

    STOPWATCH ProfessorMagic

--- app/src/main/java/com/example/sportstimer/Easteregg.java
+++ app/src/main/java/com/example/sportstimer/Easteregg.java
@@ -3,6 +3,7 @@ package com.example.sportstimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
@@ -14,8 +15,24 @@ public class Easteregg extends AppCompatActivity {
         setContentView(R.layout.activity_easteregg);
     }
 
-    public void HURRYHURRY(View view) {
+
+    public void TrueButton(View view) {
         Intent toBackMain = new Intent(this,MainActivity.class);
         startActivity(toBackMain);
+    }
+
+    public void HURRYHURRY(View view) {
+        Intent toLink = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.kuas.ac.jp/academics/faculty/engineering/Department-of-Mechanical-and-Electrical-System-Engineering"));
+        startActivity(toLink);
+    }
+
+    public void ProfessorMagic(View view) {
+        Intent toMagic = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?time_continue=2&v=53REWvxVHow&feature=emb_logo"));
+        startActivity(toMagic);
+    }
+
+    public void ProfessorMagic2(View view) {
+        Intent toMagic2 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=XMHC8qaSbZw&feature=emb_logo"));
+        startActivity(toMagic2);
     }
 }
